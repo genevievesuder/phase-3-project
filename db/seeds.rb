de315adb6ddf063837016999b9b6b98d47a3acfd
Genevieve Suder <genevievesuder@gmail.com>
2023-01-22 12:49:07 -0800

    Added to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,13 @@
+puts "Deleting existing seed data..."
 Client.destroy_all
 Hotel.destroy_all
 Reservation.destroy_all
+
+puts "Creating Hotel..."
+
+puts "Creating Clients..."
+
+puts "Creating Reservations..."
+
+puts "Seeding Complete..."
 
